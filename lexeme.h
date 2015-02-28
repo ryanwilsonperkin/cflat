530fc66332385f51f24d971b01a931fc6b21bac8
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-28 12:20:57 -0500

    Reserved keywords as lexeme type enumation

---
+++ lexeme.h
@@ -0,0 +1,18 @@
+#ifndef CFLAT_LEXEME_H
+#define CFLAT_LEXEME_H
+enum lexeme_type {
+        /* Reserved keywords */
+        CHAR,
+        ELSE,
+        FLOAT,
+        FOR,
+        IF,
+        INT,
+        RETURN,
+        STRUCT,
+        TYPEDEF,
+        SIZEOF,
+        VOID,
+        WHILE
+};
+#endif  /* CFLAT_LEXEME_H */
