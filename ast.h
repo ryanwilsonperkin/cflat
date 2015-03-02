bcc54e08df668f0e8f9e82df9df11191b5d70892
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-03-02 10:55:37 -0500

    Add struct postfix_expr_t.

--- ast.h
+++ ast.h
@@ -1,6 +1,25 @@
 #ifndef CFLAT_AST_H
 #define CFLAT_AST_H
 
+struct postfix_expr_t {
+        enum {
+                VAR,
+                CONSTANT,
+                POST_INCREMENT,
+                POST_DECREMENT,
+                ENCLOSED,
+                FUNCTION_CALL
+        } type;
+        union {
+                struct var_t *var;
+                struct constant_t *constant;
+                struct postfix_expr_t *post_increment;
+                struct postfix_expr_t *post_decrement;
+                struct expr_t *enclosed;
+                struct function_call_t *function_call;
+        } val;
+}
+
 struct var_t {
         enum {
                 IDENTIFIER,
