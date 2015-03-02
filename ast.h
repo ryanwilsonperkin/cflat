1738e1f045141a4127a631344a07778709b2aa5d
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-03-02 10:32:13 -0500

    Add value union.

--- ast.h
+++ ast.h
@@ -1,6 +1,12 @@
 #ifndef CFLAT_AST_H
 #define CFLAT_AST_H
 
+union value {
+        unsigned char cval;
+        float fval;
+        signed int ival;
+}
+
 enum basic_type {
         CHAR_TYPE,
         FLOAT_TYPE,
