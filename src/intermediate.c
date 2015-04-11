552408590016c93e7d409b7b82cd63c7482c1926
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-04-11 09:55:39 -0400

    Parse offset instructions for subscripted vars.

--- src/intermediate.c
+++ src/intermediate.c
@@ -459,8 +459,7 @@ struct quad_address *parse_instructions_identifier_var
 struct quad_address *parse_instructions_field_var
 (struct symbol_table *global, struct symbol_table *local, struct instructions *instructions, struct var *this)
 {
-        struct quad_address *parent, *parent_addr, *offset, *result_addr, *result;
-        struct quad *parent_to_addr, *result_from_addr;
+        struct quad_address *offset, *parent, *parent_addr, *result, *result_addr;
         struct symbol *parent_symbol;
         unsigned int offset_size;
         parent_symbol = translate_var(global, local, this->val.field.var);
@@ -471,6 +470,7 @@ struct quad_address *parse_instructions_field_var
         offset = create_quad_address_const_int(offset_size);
         result_addr = get_next_temp(instructions);
         add_instruction(instructions, create_quad_binary_assign(parent_addr, offset, result_addr, QUAD_OP_ADD));
+        result = get_next_temp(instructions);
         add_instruction(instructions, create_quad_copy_to_addr(result_addr, result));
         return result;
 }
@@ -478,4 +478,20 @@ struct quad_address *parse_instructions_field_var
 struct quad_address *parse_instructions_subscript_var
 (struct symbol_table *global, struct symbol_table *local, struct instructions *instructions, struct var *this)
 {
+        struct quad_address *index, *offset, *parent, *parent_addr, *parent_size, *result, *result_addr;
+        struct quad *calculate_offset;
+        struct symbol *parent_symbol;
+        parent_symbol = translate_var(global, local, this->val.subscript.var);
+        parent_size = create_quad_address_const_int(parent_symbol->size);
+        parent = parse_instructions_var(global, local, instructions, this->val.subscript.var);
+        index = parse_instructions_expr(global, local, instructions, this->val.subscript.expr);
+        parent_addr = get_next_temp(instructions);
+        add_instruction(instructions, create_quad_copy_from_addr(parent, parent_addr));
+        offset = get_next_temp(instructions);
+        add_instruction(instructions, create_quad_binary_assign(parent_size, index, offset, QUAD_OP_MULTIPLY)); 
+        result_addr = get_next_temp(instructions);
+        add_instruction(instructions, create_quad_binary_assign(parent_addr, offset, result_addr, QUAD_OP_ADD));
+        result = get_next_temp(instructions);
+        add_instruction(instructions, create_quad_copy_from_addr(result_addr, result));
+        return result;
 }
