2a2665177e23bcc783f872fdfabe5d9e1cbf7f26
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-04-12 10:06:35 -0400

    Use local symbol table in code gen.

--- src/asm.c
+++ src/asm.c
@@ -48,63 +48,70 @@ struct assembly *parse_assembly
 (struct symbol_table *global, struct instructions *instructions)
 {
         int i;
+        struct symbol *symbol;
+        struct quad *quad;
         struct assembly *this = create_assembly();
+        struct symbol_table *local = global;
         for (i = 0; i < instructions->n_quads; i++) {
-                parse_assembly_instruction(global, this, instructions->quads[i]);
+                quad = instructions->quads[i];
+                switch (quad->type) {
+                case QUAD_BINARY_ASSIGN:
+                        parse_assembly_binary_assign(global, local, this, quad);
+                        break;
+                case QUAD_UNARY_ASSIGN:
+                        parse_assembly_unary_assign(global, local, this, quad);
+                        break;
+                case QUAD_COPY:
+                        parse_assembly_copy(global, local, this, quad);
+                        break;
+                case QUAD_COPY_ADDR:
+                        parse_assembly_copy_addr(global, local, this, quad);
+                        break;
+                case QUAD_COPY_FROM_ADDR:
+                        parse_assembly_copy_from_addr(global, local, this, quad);
+                        break;
+                case QUAD_COPY_TO_ADDR:
+                        parse_assembly_copy_to_addr(global, local, this, quad);
+                        break;
+                case QUAD_LABEL:
+                        symbol = get_symbol(global, quad->val.label.label);
+                        if (symbol && symbol->type == SYMBOL_FUNCTION) {
+                                local = symbol->scoped_table;
+                        }
+                        parse_assembly_label(global, local, this, quad);
+                        break;
+                case QUAD_UNCONDITIONAL_JUMP:
+                        parse_assembly_unconditional_jump(global, local, this, quad);
+                        break;
+                case QUAD_CONDITIONAL_JUMP:
+                        parse_assembly_conditional_jump(global, local, this, quad);
+                        break;
+                case QUAD_RELATIONAL_JUMP:
+                        parse_assembly_relational_jump(global, local, this, quad);
+                        break;
+                case QUAD_PROCEDURE_PARAM:
+                        parse_assembly_procedure_param(global, local, this, quad);
+                        break;
+                case QUAD_PROCEDURE_CALL:
+                        parse_assembly_procedure_call(global, local, this, quad);
+                        break;
+                case QUAD_PROCEDURE_RETURN:
+                        parse_assembly_procedure_return(global, local, this, quad);
+                        break;
+                default:
+                        assert(0);  /* Invalid enum value. */
+                }
         }
         return this;
 }
 
 void parse_assembly_instruction
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
-        switch (this->type) {
-        case QUAD_BINARY_ASSIGN:
-                parse_assembly_binary_assign(global, assembly, this);
-                break;
-        case QUAD_UNARY_ASSIGN:
-                parse_assembly_unary_assign(global, assembly, this);
-                break;
-        case QUAD_COPY:
-                parse_assembly_copy(global, assembly, this);
-                break;
-        case QUAD_COPY_ADDR:
-                parse_assembly_copy_addr(global, assembly, this);
-                break;
-        case QUAD_COPY_FROM_ADDR:
-                parse_assembly_copy_from_addr(global, assembly, this);
-                break;
-        case QUAD_COPY_TO_ADDR:
-                parse_assembly_copy_to_addr(global, assembly, this);
-                break;
-        case QUAD_LABEL:
-                parse_assembly_label(global, assembly, this);
-                break;
-        case QUAD_UNCONDITIONAL_JUMP:
-                parse_assembly_unconditional_jump(global, assembly, this);
-                break;
-        case QUAD_CONDITIONAL_JUMP:
-                parse_assembly_conditional_jump(global, assembly, this);
-                break;
-        case QUAD_RELATIONAL_JUMP:
-                parse_assembly_relational_jump(global, assembly, this);
-                break;
-        case QUAD_PROCEDURE_PARAM:
-                parse_assembly_procedure_param(global, assembly, this);
-                break;
-        case QUAD_PROCEDURE_CALL:
-                parse_assembly_procedure_call(global, assembly, this);
-                break;
-        case QUAD_PROCEDURE_RETURN:
-                parse_assembly_procedure_return(global, assembly, this);
-                break;
-        default:
-                assert(0);  /* Invalid enum value. */
-        }
 }
 
 void parse_assembly_binary_assign
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
         struct quad_address *arg1, *arg2, *result;
         int reg_arg1, reg_arg2, reg_result;
@@ -126,62 +133,62 @@ void parse_assembly_binary_assign
 }
 
 void parse_assembly_unary_assign
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_copy
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_copy_addr
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_copy_from_addr
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_copy_to_addr
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_label
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_unconditional_jump
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_conditional_jump
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_relational_jump
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_procedure_param
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_procedure_call
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
 void parse_assembly_procedure_return
-(struct symbol_table *global, struct assembly *assembly, struct quad *this)
+(struct symbol_table *global, struct symbol_table *local, struct assembly *assembly, struct quad *this)
 {
 }
 
